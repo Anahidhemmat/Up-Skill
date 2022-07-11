fc7ed1f9844214ca42fe5a9f587f8af75717231c
Anahid <anahidhemmat2130@gmail.com>
2022-07-12 02:01:39 +0400

    done

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,30 @@
 import "./App.css";
 import ButtonComponent from "./components/Button";
+import CardComponent from "./components/Card";
+import TextFieldComponent from "./components/Input";
 import NavbarComponent from "./components/Navbar";
 
 function App() {
   return (
     <div>
       <NavbarComponent />
+      <TextFieldComponent label="name" variant="outlined" />
+      {/* <TextFieldComponent label="name" variant="filled" />
+      <TextFieldComponent label="name" variant="standard" /> */}
+      <CardComponent
+        title="React"
+        cardBody="Search for the keywords to learn more about each warning.
+To ignore, add // eslint-disable-next-line to the line before."
+        btn1="share"
+        btn2="learn more"
+        src="https://source.unsplash.com/random"
+        alt="unsplash img"
+      />
       <ButtonComponent
-        text={"Click me!"}
-        type={"button"}
-        variant={"outlined"}
-        color={"secondary"}
+        text="Click me!"
+        type="button"
+        variant="outlined"
+        color="secondary"
       />
     </div>
   );
