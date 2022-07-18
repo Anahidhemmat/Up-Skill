693ace56b224d688480f5eb2c710f73d8911473d
Anahid <anahidhemmat2130@gmail.com>
2022-07-18 19:06:00 +0400

    changes

--- client/src/components/Input/Input.js
+++ client/src/components/Input/Input.js
@@ -3,17 +3,13 @@ import { TextField, Stack } from "@mui/material";
 
 const TextFieldComponent = ({ label, variant, size, type, placeholder }) => {
   return (
-    <Stack spacing={4}>
-      <Stack direction="row" spacing={2}>
-        <TextField
-          label={label}
-          variant={variant}
-          size={size}
-          type={type}
-          placeholder={placeholder}
-        />
-      </Stack>
-    </Stack>
+    <TextField
+      label={label}
+      variant={variant}
+      size={size}
+      type={type}
+      placeholder={placeholder}
+    />
   );
 };
 
