98044fa0a1476a6ff840cc3d64a268b590f0f624
Anahid <anahidhemmat2130@gmail.com>
2022-07-20 02:36:56 +0400

    fix bug

---
+++ client/src/components/List/List.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Box, List } from "@mui/material";
+import { bgcolor } from "@mui/system";
+import ListItemComponent from "../ListItem";
+
+const ListComponent = ({ width, children, bgcolor }) => {
+  return (
+    <Box width={width} bgcolor={bgcolor}>
+      <List>{children}</List>
+    </Box>
+  );
+};
+
+export default ListComponent;
