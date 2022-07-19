b413c05b41a36be6aaff575bfbda4dca75ca5db3
Anahid <anahidhemmat2130@gmail.com>
2022-07-20 02:36:46 +0400

    list tests

---
+++ client/src/components/List/__test__/List.test.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import ListComponent from "../List";
+
+it("should render list component", async () => {
+  render(<ListComponent />);
+  const list = screen.getByRole("list");
+  expect(list).toBeInTheDocument();
+});
