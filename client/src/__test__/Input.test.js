994f1f665185ba62b0c16ac7d6ebca8447899ff5
Anahid <anahidhemmat2130@gmail.com>
2022-07-21 22:32:56 +0400

    Input tests

--- client/src/__test__/Input.test.js
+++ client/src/__test__/Input.test.js
@@ -2,41 +2,34 @@ import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
 import TextFieldComponent from "../components/Input/Input";
 
-describe("should have props", () => {
-  it("should render textbox", async () => {
-    render(<TextFieldComponent onChange={() => {}} />);
-    const inputElement = screen.getByRole("textbox");
-    fireEvent.change(inputElement);
-    expect(inputElement).toBeInTheDocument();
-  });
+it("should render correctly", () => {
+  render(<TextFieldComponent />);
+  const inputElement = screen.getByRole("textbox");
+  expect(inputElement).toBeTruthy();
+});
 
-  it("should render same label passed into label prop", () => {
-    render(<TextFieldComponent label={""} />);
-    const inputElement = screen.getByRole("textbox");
-    expect(inputElement).toBeInTheDocument();
-  });
+it("updates onChange", () => {
+  const mockOnChange = jest.fn();
+  render(<TextFieldComponent onChange={mockOnChange} />);
+  const inputElement = screen.getByRole("textbox");
+  fireEvent.change(inputElement, { target: { value: "test" } });
+  expect(inputElement.value).toBe("test");
+});
 
-  it("should render same variant passed into variant prop", () => {
-    render(<TextFieldComponent />);
-    const inputElement = screen.getByRole("textbox");
-    expect(inputElement).toBeInTheDocument();
-  });
+it("should render type attribute", () => {
+  render(<TextFieldComponent type="text" />);
+  const inputElement = screen.getByRole("textbox");
+  expect(inputElement).toHaveAttribute("type", "text");
+});
 
-  it("should render same size passed into size prop", () => {
-    render(<TextFieldComponent size={""} />);
-    const inputElement = screen.getByRole("textbox");
-    expect(inputElement).toBeInTheDocument();
-  });
+it("should render placeholder attribute", () => {
+  render(<TextFieldComponent placeholder="write your name" />);
+  const inputElement = screen.getByRole("textbox");
+  expect(inputElement).toHaveAttribute("placeholder", "write your name");
+});
 
-  it("should render same type passed into type prop", () => {
-    render(<TextFieldComponent type={""} />);
-    const inputElement = screen.getByRole("textbox");
-    expect(inputElement).toBeInTheDocument();
-  });
-
-  it("should render same placeholder passed into placeholder prop", () => {
-    render(<TextFieldComponent placeholder={""} />);
-    const inputElement = screen.getByRole("textbox");
-    expect(inputElement).toBeInTheDocument();
-  });
+it("should render props", () => {
+  render(<TextFieldComponent label="name" variant="outlined" size="small" />);
+  const inputElement = screen.getByRole("textbox");
+  expect(inputElement).toBeTruthy();
 });
