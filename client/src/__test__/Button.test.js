452b69e6ad1013f8fafdd0d3e1bcf2d286d6d946
Anahid <anahidhemmat2130@gmail.com>
2022-07-21 19:44:16 +0400

    button tests

---
+++ client/src/__test__/Button.test.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import ButtonComponent from "../components/Button/Button";
+
+it("should render button", async () => {
+  render(<ButtonComponent />);
+  const buttonElement = screen.getByRole("button");
+  expect(buttonElement).toBeInTheDocument();
+});
+
+it("should render button text", async () => {
+  render(<ButtonComponent>Click</ButtonComponent>);
+  const buttonElement = screen.getByText(/Click/i);
+  expect(buttonElement.textContent).toBe("Click");
+});
+
+it("should be disabled", async () => {
+  render(<ButtonComponent disabled />);
+  const buttonElement = screen.getByRole("button");
+  expect(buttonElement).toBeDisabled();
+});
+
+it("should have onClick event", async () => {
+  const mockHanleClick = jest.fn();
+  render(<ButtonComponent onClick={mockHanleClick} />);
+  const buttonElement = screen.getByRole("button");
+  fireEvent.click(buttonElement);
+  expect(mockHanleClick).toBeCalled();
+});
+
+it("should not be disabled", async () => {
+  render(<ButtonComponent />);
+  const buttonElement = screen.getByRole("button");
+  expect(buttonElement).not.toBeDisabled();
+});
+
+it("should have type attribute", async () => {
+  render(<ButtonComponent type="button" />);
+  const buttonElement = screen.getByRole("button");
+  expect(buttonElement).toHaveAttribute("type", "button");
+});
